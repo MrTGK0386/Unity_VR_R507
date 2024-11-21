74daf7ba5bea01c26311b866ae54ce73ec474ba7
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2024-11-22 00:07:48 +0100

    MOD : Ajout des script sur les light
    modif du script emissive light pour que les ampoules ne perde pas leur couleur emisive à l'arret de la scene (ou reload)

--- Assets/Scripts/Aesthetic/EmissiveLightSync.cs
+++ Assets/Scripts/Aesthetic/EmissiveLightSync.cs
@@ -5,18 +5,34 @@ public class EmissiveLightSync : MonoBehaviour
 {
     [SerializeField] private Material emissiveMaterial;
     private Light lightComponent;
-    private Color emissionColor;
+    private Color originalEmissionColor;
+    
+    private void Awake()
+    {
+        // Sauvegarder la couleur d'émission originale au tout début
+        originalEmissionColor = emissiveMaterial.GetColor("_EmissionColor");
+    }
     
     private void Start()
     {
         lightComponent = GetComponent<Light>();
-        // Sauvegarde la couleur d'émission configurée dans Unity
-        emissionColor = emissiveMaterial.GetColor("_EmissionColor");
     }
 
     private void Update()
     {
-        // Active/désactive l'émission en fonction de l'état de la lumière
-        emissiveMaterial.SetColor("_EmissionColor", lightComponent.enabled ? emissionColor : Color.black);
+        // Activer/désactiver l'émission en fonction de l'état de la lumière
+        emissiveMaterial.SetColor("_EmissionColor", lightComponent.enabled ? originalEmissionColor : Color.black);
+    }
+
+    private void OnDisable()
+    {
+        // Restaurer la couleur originale quand le script est désactivé
+        emissiveMaterial.SetColor("_EmissionColor", originalEmissionColor);
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Restaurer la couleur originale quand l'application se ferme
+        emissiveMaterial.SetColor("_EmissionColor", originalEmissionColor);
     }
 }
